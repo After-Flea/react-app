adf7fd699cb1d027cec2a10846487342ea798420
emmanuelakanji <132822251+emmanuelakanji@users.noreply.github.com>
2023-05-13 02:38:07 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
 import './Home.css';
